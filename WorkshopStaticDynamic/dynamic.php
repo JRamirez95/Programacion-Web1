e953122fd0cccc77857c1c2859ca09c1f84f1d6c
JRamirez95 <jona16121995@gmail.com>
2017-10-11 22:49:26 -0600

    Dynamic vs Static
    Workshop

---
+++ WorkshopStaticDynamic/dynamic.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+     <title>Dynamic</title>
+    </head>
+    <body>
+    <?php 
+     $saludo = "HELLO WORLD";
+    ?>
+
+    <img src="http://ajra.es/wp-content/uploads/Evolucion-Desarrollo-Web-01.gif" alt="">
+
+<span ><?php echo $saludo; ?></span>
+
+    </body>
+   </html>
