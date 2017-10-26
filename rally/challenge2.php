adea0a8e77b5ba97605228d6a39a9e927505e38a
JRamirez95 <jona16121995@gmail.com>
2017-10-25 21:52:08 -0600

    Correción
    Se estaba trabajando en el hdocs y se guardaron los cambios en dicha carpeta, la correcion es que se copio el contenido del challeng2.php de hdocs en la carpeta de rally

--- rally/challenge2.php
+++ rally/challenge2.php
@@ -1,4 +1,3 @@
-
 <?php
 
 
@@ -11,16 +10,22 @@ $txt = $argv[1];
 
 // Consultar la base de datos
 $con = mysqli_connect("localhost","root","","rally") or die ("Error de conexion");
-$consulta = "SELECT e.cedula, e.nombre, e.apellidos, c.nombre, es.fecha FROM estudiante as e, estudiante_carrera as es, carrera as c WHERE e.cedula = es.id_estudiante AND c.nombre = es.id_carrera";
+$consulta = "SELECT e.cedula, e.nombre, e.apellidos, c.descripcion, es.fecha FROM estudiante as e, estudiante_carrera as es, carrera as c WHERE e.cedula = es.id_estudiante AND c.descripcion = es.id_carrera";
 $ejecutar = mysqli_query($con,$consulta);
 
 
 
 while ($row = mysqli_fetch_assoc($ejecutar)) {
     print_r($row);
+ 
+   $cedula = $row['cedula'];
+   $nombre = $row['nombre'];
+   $apellidos = $row['apellidos'];
+   $carrera = $row['descripcion'];
+   $fecha = $row['fecha'];
 
     $file = fopen($txt, "a");
-    fwrite($file, "$row\n" .PHP_EOL);
+    fwrite($file, "$cedula, $nombre, $apellidos, $carrera, $fecha\n" .PHP_EOL);
     fclose($file);   
     
 }    
